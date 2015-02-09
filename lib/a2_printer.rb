168d0eed08b62352b2b8ea1567308c89745c42f9
Sergio Revilla <replymusic@gmail.com>
2015-02-09 13:09:54 +0100

    snapshot at Mon Feb  9 12:40:46 CET 2015

--- lib/a2_printer.rb
+++ lib/a2_printer.rb
@@ -108,8 +108,6 @@ class A2Printer
       bitmap = Bitmap.new *args
     end
 
-    bitmap = Bitmap.from_source args[0] if only_source_provided
-    bitmap = Bitmap.new(*args) if args.size != 1
     return if (bitmap.width > 384) # maximum width of the printer
     bitmap.each_block do |w, h, bytes|
       write_bytes(18, 42)
