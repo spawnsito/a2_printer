746ce404629457bfbaa0de7aba9e61bff14f3b74
Sergio Revilla <replymusic@gmail.com>
2015-02-16 11:56:44 +0100

    snapshot at Mon Feb 16 11:33:28 CET 2015

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -23,13 +23,13 @@ class Bitmap
       width_in_bytes = @width / 8
 
       while row_start < @height do
-        chunk_height = ((@height - row_start) > 255) ? 255 : (@height - row_start)
+        chunk_height = ((@height - row_start) > MAX_BYTE) ? MAX_BYTE : (@height - row_start)
         bytes = (0...(width_in_bytes * chunk_height)).map { @data.getbyte }
 
         connection.write_bytes(18, 42)
         connection.write_bytes(chunk_height, width_in_bytes)
         connection.write_bytes(*bytes)
-        row_start += 255
+        row_start += MAX_BYTE
       end
     end
 
