b318a4db35383eb67736cce2dc9a81cb0bd94c55
Sergio Revilla <replymusic@gmail.com>
2015-02-16 12:05:06 +0100

    snapshot at Mon Feb 16 11:33:28 CET 2015

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -42,7 +42,7 @@ class Bitmap
     def calculate_chunk_height row_start
       ((@height - row_start) > MAX_BYTE) ? MAX_BYTE : (@height - row_start)
 
-      chunk_height
+      chunk_height = @height - row_start
 
       if (@height - row_start) > MAX_BYTE)
         MAX_BYTE
